131ed57bdf76258c49aceb4d18ceb2e4096df04d
Catevika <dominique.bello@outlook.fr>
2019-07-13 18:57:45 +0200

    Updates

---
+++ precache-manifest.0140fb758a14050f9a83531418a7b3ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c78da6569aa7ced3737e48b298f97e",
+    "url": "/UDEMY_ToDo_App_withHooks/index.html"
+  },
+  {
+    "revision": "2a885ccde926b7eecdf0",
+    "url": "/UDEMY_ToDo_App_withHooks/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "615448da0cf0fb8753bb",
+    "url": "/UDEMY_ToDo_App_withHooks/static/js/2.3a43d96c.chunk.js"
+  },
+  {
+    "revision": "2a885ccde926b7eecdf0",
+    "url": "/UDEMY_ToDo_App_withHooks/static/js/main.058eea53.chunk.js"
+  },
+  {
+    "revision": "962594942f2ecb1c565c",
+    "url": "/UDEMY_ToDo_App_withHooks/static/js/runtime~main.53b214ed.js"
+  }
+]);
